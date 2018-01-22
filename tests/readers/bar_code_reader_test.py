741fe2f81112ba4cb82b4db9c8db2f3296bbde61
dsola <d.sola.03@gmail.com>
2018-01-22 23:14:23 +0100

    Specify relative path to create test assets for testing

--- tests/readers/bar_code_reader_test.py
+++ tests/readers/bar_code_reader_test.py
@@ -1,19 +1,28 @@
 import unittest
 
+from exceptions.invalid_directory_path_exception import InvalidDirectoryPathException
 from tiqets.readers.bar_code_reader import BarCodeReader
-from pprint import pprint
+
+TEST_ASSETS_DIRECTORY = "/../../tests/assets/"
 
 class BarCodeReaderTest(unittest.TestCase):
 
+    def test_exception_raise_when_directory_not_exists(self):
+        self.assertRaises(InvalidDirectoryPathException,BarCodeReader.read, TEST_ASSETS_DIRECTORY + "not_exists.csv")
+
     def test_list_is_not_empty(self):
-        bar_codes = BarCodeReader.read()
+        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "one_barcode.csv")
         self.assertGreater(len(bar_codes), 0)
 
     def test_check_specific_bar_code(self):
-        bar_codes = BarCodeReader.read()
-        bar_code = bar_codes[2]
-        self.assertEquals('11111111113', bar_code.get_reference())
-        self.assertEquals('3', bar_code.get_order_id())
+        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "one_barcode.csv")
+        bar_code = bar_codes[0]
+        self.assertEquals('11111111111', bar_code.get_reference())
+        self.assertEquals('1', bar_code.get_order_id())
+
+    def test_read_multiple_barcodes(self):
+        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "barcodes.csv")
+        self.assertEquals(len(bar_codes), 7)
 
 
 if __name__ == '__main__':
--- tiqets/readers/bar_code_reader.py
+++ tiqets/readers/bar_code_reader.py
@@ -1,17 +1,28 @@
 import csv, os
+
+from exceptions.invalid_directory_path_exception import InvalidDirectoryPathException
 from tiqets.entities.bar_code import BarCode
 
-FILE_PATH = "/../../assets/barcodes.csv"
+DEFAULT_FILE_PATH = "/../../assets/barcodes.csv"
 
 class BarCodeReader:
 
     @staticmethod
-    def read():
+    def read(relative_path = ""):
+        relative_path = BarCodeReader.get_relative_path(relative_path)
         bar_codes = []
-        with open(os.path.dirname(__file__) + FILE_PATH, 'r') as file:
+        with open(os.path.dirname(__file__) + relative_path, 'r') as file:
             reader = csv.reader(file, delimiter=',')
             next(reader)
             for row in reader:
                 bar_codes.append(BarCode(row[0], row[1]))
 
         return bar_codes
+
+    @staticmethod
+    def get_relative_path(relative_path):
+        if (relative_path == ""):
+            return DEFAULT_FILE_PATH
+        elif not (os.path.exists(os.path.dirname(__file__) + relative_path)):
+            raise InvalidDirectoryPathException("Any file found in the path " + relative_path)
+        return relative_path
