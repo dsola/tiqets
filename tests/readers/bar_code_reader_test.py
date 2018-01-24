7c57b80c63846498a1b3d70beb8809c4af4f7178
dsola <d.sola.03@gmail.com>
2018-01-25 00:01:40 +0100

    Implement tests to cover the number of bar_codes without any order

--- tests/readers/bar_code_reader_test.py
+++ tests/readers/bar_code_reader_test.py
@@ -11,25 +11,29 @@ class BarCodeReaderTest(unittest.TestCase):
         self.assertRaises(InvalidDirectoryPathException,BarCodeReader.read, TEST_ASSETS_DIRECTORY + "not_exists.csv")
 
     def test_list_is_not_empty(self):
-        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "one_barcode.csv")
+        (bar_codes, bar_codes_without_order) = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "one_barcode.csv")
         self.assertGreater(len(bar_codes), 0)
+        self.assertEquals(len(bar_codes_without_order), 0)
 
     def test_check_specific_bar_code(self):
-        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "one_barcode.csv")
+        (bar_codes, bar_codes_without_order) = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "one_barcode.csv")
         bar_code = bar_codes[0]
         self.__assert_bar_code(bar_code)
 
     def test_read_multiple_barcodes(self):
-        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "barcodes.csv")
+        (bar_codes, bar_codes_without_order) = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "barcodes.csv")
         self.assertEquals(len(bar_codes), 7)
+        self.assertEquals(len(bar_codes_without_order), 0)
 
     def test_orders_without_barcodes_are_not_included(self):
-        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "some_incorrect_barcodes.csv")
+        (bar_codes, bar_codes_without_order) = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "some_incorrect_barcodes.csv")
         self.assertEquals(len(bar_codes), 5)
+        self.assertEquals(len(bar_codes_without_order), 2)
 
     def test_any_bar_code_repeated_has_been_included(self):
-        bar_codes = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "barcodes_with_duplicates.csv")
+        (bar_codes, bar_codes_without_order) = BarCodeReader.read(TEST_ASSETS_DIRECTORY + "barcodes_with_duplicates.csv")
         self.assertEquals(len(bar_codes), 5)
+        self.assertEquals(len(bar_codes_without_order), 0)
 
     def __assert_bar_code(self, bar_code):
         self.assertEquals('11111111111', bar_code.get_reference())
