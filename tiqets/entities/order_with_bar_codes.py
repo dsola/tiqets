1995cfe4465b94906564e004d1b5830ed74ac59d
dsola <d.sola.03@gmail.com>
2018-01-25 19:32:04 +0100

    Implement heritance in the orders

--- tiqets/entities/order_with_bar_codes.py
+++ tiqets/entities/order_with_bar_codes.py
@@ -1,14 +1,10 @@
-class OrderWithBarCodes(object):
-    def __init__(self, Id, CustomerId, BarCodes):
-        self.__id = Id
-        self.__customer_id = CustomerId
-        self.__bar_codes=BarCodes
+from entities.order import Order
 
-    def get_id(self):
-        return self.__id
+
+class OrderWithBarCodes(Order):
+    def __init__(self, Id, CustomerId, BarCodes):
+        super().__init__(Id, CustomerId)
+        self.__bar_codes=BarCodes
 
     def get_bar_code_references(self):
         return self.__bar_codes
-
-    def get_customer_id(self):
-        return self.__customer_id
